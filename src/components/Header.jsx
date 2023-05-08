f3edd2f978451b100c01f52a89f7f22f357e9a01
ReneTorresA <rene.torres.a@usach.cl>
2023-05-08 00:52:53 -0400

    changed urls in /cursos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <div className="flex justify-center py-3">
-      <Link to="/cursos/cursos">
+      <Link to="/cursos">
         <h1 className="font-bold uppercase text-3xl mb-4">Asignaturas</h1>
       </Link>
     </div>
